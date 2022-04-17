085a5cd163807222bcd7ccb573e70b85df8d5ff9
EunseokOh <lspk.lpg@gmail.com>
2022-04-17 20:04:48 +0900

    entity ddl test

--- src/main/kotlin/hellojpa/JpaMain.kt
+++ src/main/kotlin/hellojpa/JpaMain.kt
@@ -4,19 +4,19 @@ import javax.persistence.EntityManagerFactory
 import javax.persistence.Persistence
 
 fun main(args: Array<String>){
-    var emf = Persistence . createEntityManagerFactory ("hello");
+    val emf = Persistence . createEntityManagerFactory ("hello");
 
-    var em = emf.createEntityManager();
+    val em = emf.createEntityManager();
 
-    var tx = em.transaction;
+    val tx = em.transaction;
     tx.begin();
 
     try{
-        var result = em.createQuery("select m from Member as m", Member::class.java).resultList;
-
-        for(member in result){
-            println("member.name" + member.name);
-        }
+        val member = Member();
+        member.id = 1L;
+        member.name = "helloJpa";
+        member.roleType = RoleType.USER;
+        em.persist(member);
 
         tx.commit();
     }catch (e : java.lang.Exception){
--- src/main/kotlin/hellojpa/Member.kt
+++ src/main/kotlin/hellojpa/Member.kt
@@ -1,11 +1,35 @@
 package hellojpa
 
-import javax.persistence.Entity
-import javax.persistence.Id
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.*
 
 @Entity
 class Member {
     @Id
     var id: Long? = null;
+
+    @Column(name = "name", nullable = false)
     var name:String = "";
+
+    var age: Int = 0;
+
+    @Enumerated(EnumType.STRING)
+    lateinit var roleType: RoleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    lateinit var createdDate: Date;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    lateinit var lastModifiedDate: Date;
+
+    lateinit var testLocaldate: LocalDate;
+    lateinit var testLocalDateTime : LocalDateTime
+
+    @Lob
+    lateinit var description: String;
+
+    @Transient
+    var systemValue: Int = 0;
 }
